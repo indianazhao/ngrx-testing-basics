2b81337c0240187f700c47a99b6adde179b3e2f7
indianazhao <indiana.zhao@gmail.com>
2022-07-05 22:46:27 +0800

    Part-17 Testing a getter selector (完成)

--- src/app/_state/llamas/llamas.selectors.spec.ts
+++ src/app/_state/llamas/llamas.selectors.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { SubscriberSpy, subscribeSpyTo } from '@hirez_io/observer-spy';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { SubscriberSpy } from '@hirez_io/observer-spy';
+import { createFakeLlama } from 'src/app/_types/llama.fake';
+import { selectLlamas } from './llamas.selectors';
 import { Llama } from './../../_types/llama.type';
 import { AppState } from './../app-state.type';
 
@@ -18,5 +20,26 @@ describe('LlamasSelectors', () => {
 
     fakeLlamas = undefined;
     observerSpy = undefined;
+  });
+
+  describe('SELECTOR: selectLlamas', () => {
+    Given(() => {
+      fakeLlamas = [createFakeLlama()];
+
+      // 如果直接使用下面這行直接指定內容，會被要求 router 內容 (得再去建立一個 router spy...)，
+      // selectLlamas({ llamas: fakeLlamas, router: {} });
+      // 所以改用 storeSpy (這也是為什麼上面要宣告型別為 Parital 的原因)
+      storeSpy.setState({
+        llamas: fakeLlamas,
+      });
+    });
+
+    When(() => {
+      observerSpy = subscribeSpyTo(storeSpy.select(selectLlamas));
+    });
+
+    Then('should return the llamas', () => {
+      expect(observerSpy.getLastValue()).toEqual(fakeLlamas);
+    });
   });
 });
